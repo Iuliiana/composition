3a2ccdbea6a06d732a1f8df76a6ae6d10b4805da
Iuliia <tretiakova.iuliia@gmail.com>
2022-08-08 23:09:50 +0300

    Декомпозиция

---
+++ src/data/newsNav.js
@@ -0,0 +1,19 @@
+const newsNav = [
+    {
+        id: 1,
+        title: 'Сейчас в СМИ',
+        link: 'https://ru.lipsum.com/',
+    },
+    {
+        id: 2,
+        title: 'В Германии',
+        link: 'https://ru.lipsum.com/',
+    },
+    {
+        id: 3,
+        title: 'Рекомендуем',
+        link: 'https://ru.lipsum.com/',
+    },
+];
+
+export default newsNav;
